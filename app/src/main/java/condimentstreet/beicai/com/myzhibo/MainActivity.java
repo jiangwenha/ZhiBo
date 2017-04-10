0d1f71ab19d420a2d72a81f48c8f1595d4e4c20d
jiangwenha <3318219211@qq.com>
2017-04-10 13:55:22 +0800

    first ZhiBo

--- app/src/main/java/condimentstreet/beicai/com/myzhibo/MainActivity.java
+++ app/src/main/java/condimentstreet/beicai/com/myzhibo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"dfhjnv",Toast.LENGTH_LONG);
-
+        Toast.makeText(this,"姜文浩",Toast.LENGTH_LONG);
     }
 }
